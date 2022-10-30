b920ee648e258633a2fe4f7f6ecc3155fe375587
JuanPenalverSanchez <82878367+JuanPenalverSanchez@users.noreply.github.com>
2022-10-30 18:02:02 +0100

    Tranformacion y representacion de datos

--- App.tsx
+++ App.tsx
@@ -1,11 +1,51 @@
 import * as React from 'react';
+import * as xlsx from 'xlsx';
 import './style.css';
 
 export default function App() {
+  const [datosExcel, setDatosExcel] = React.useState([]);
+
+  const readUploadFile = (e) => {
+    if (e.target.files) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const data = e.target.result;
+        const workbook = xlsx.read(data, { type: 'array' });
+        const sheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[sheetName];
+        const json = xlsx.utils.sheet_to_json(worksheet);
+        setDatosExcel(json);
+      };
+      reader.readAsArrayBuffer(e.target.files[0]);
+    }
+  };
+
+  const pintarDatos = () => {
+    console.log(datosExcel);
+
+    return (
+      <ul>
+        {datosExcel.map((elemt) => (
+          <li>
+            {elemt.Tipo} - {elemt.Value}
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <form>
+        <label htmlFor="upload">Upload File</label>
+        <input
+          type="file"
+          name="upload"
+          id="upload"
+          onChange={readUploadFile}
+        />
+      </form>
+      {pintarDatos()}
     </div>
   );
 }
